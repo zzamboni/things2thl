239db32099efedbe20f19cbe0eee7d7f6568c3ac
Diego Zamboni <diego@zzamboni.org>
2009-05-19 00:23:16 +0200

    Version bump to 0.4.0, added ChangeLog
    Updated README file

--- lib/Things2THL.rb
+++ lib/Things2THL.rb
@@ -11,7 +11,7 @@ require 'appscript'; include Appscript
 module Things2THL
   module Version
     MAJOR  = 0
-    MINOR  = 3
+    MINOR  = 4
     PATCH  = 0
 
     STRING = [MAJOR, MINOR, PATCH].join(".")
